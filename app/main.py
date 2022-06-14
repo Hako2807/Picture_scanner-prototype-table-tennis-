7071be796c99862b7e64bb60284714b6b1a49b4c
Hako2807 <hakonts@icloud.com>
2022-06-14 21:32:32 +0200

    The app can now scan a directory

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-print("Hello World")
+import os
+
+picture_names = []
+
+with os.scandir('pictures/') as entries:
+    for entry in entries:
+        if entry.name.endswith(".png"):
+            picture_names.append(entry.name)
+
+print(picture_names)
