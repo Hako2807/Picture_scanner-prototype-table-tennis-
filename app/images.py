f6514fac5916b0e929d1999f2cdeb270bb9337e4
Hako2807 <hakonts@icloud.com>
2022-06-15 16:27:58 +0200

    Added code to find pixel values for colors in a range.

--- app/images.py
+++ app/images.py
@@ -21,3 +21,13 @@ class Images:
                 for x in range(img.size[0]):
                     color_values.append(img.getpixel((x, y)))
             self.rgbas.append(color_values)
+        self.find_colors()
+
+    def find_colors(self):
+        for x, col in enumerate(self.rgbas[0]):
+            if self.check_value(0, 255, 160, 165, 0, 30, col):
+                print(f"x: {x % self.imgs[0].size[1]}, y: {x // self.imgs[0].size[1]}, color: {col}")
+
+    def check_value(self, minr, maxr, ming, maxg, minb, maxb, rgb):
+        return minr <= rgb[0] <= maxr and ming <= rgb[1] <= maxg and minb <= rgb[2] <= maxb
+
